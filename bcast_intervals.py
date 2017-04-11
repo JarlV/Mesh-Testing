2033f173874740c7647a6572e8798a1958808dc0
Jarl Victor Bjørgan <JVBjorgan@gmail.com>
2017-04-11 17:34:00 +0200

    added main test file and test for trickle in single mesh node

---
+++ bcast_intervals.py
@@ -0,0 +1,45 @@
+import analyse
+
+def is_in_rx_range(time):
+    return time >= rx_range[0] and time <= rx_range[1]
+
+def is_tx_edge(i, j):
+    left_is_rx = is_in_rx_range(float(i))
+    right_is_rx = is_in_rx_range(float(j))
+    #print(float(i), float(j), "edge is tx: ", not right_is_rx and not left_is_rx)
+    if left_is_rx & right_is_rx:
+        return "Abort" #raise ValueError("Edge is ambiguous", sample_array[index], sample_array[index + 1])
+    return not right_is_rx and not left_is_rx
+
+def calculate_tx_intervals(sample_array):
+    tx_intervals = []
+    current_interval = 0
+    for i in range(len(sample_array)-1):
+        #print("calculate_tx: ", sample_array[i], sample_array[i+1])
+        if is_tx_edge(sample_array[i], sample_array[i+1]) == "Abort":
+            # print("At interval", len(tx_intervals)+1, "sample number", i, ". Edge is ambiguous.")
+            return tx_intervals
+        current_interval += float(sample_array[i])
+        if is_tx_edge(sample_array[i], sample_array[i+1]):
+            if len(tx_intervals) > 0 and current_interval < tx_intervals[-1]:
+                pass # print("At interval", len(tx_intervals)+1, "sample number", i, ". interval is smaller than last interval.")
+            tx_intervals.append(current_interval)
+            current_interval = 0
+    return tx_intervals
+
+def validate_intervals(data, imin, imax):
+    for i in data:
+        pass
+
+data = analyse.LogicData("trickle_under_5_mins.csv", "ms")
+data.set_decimal_points(4)
+# data.capture(60*5, 1, "C:/Users/JarlVictor/Dropbox/Code/2017/")
+delta = data.get_delta_times()
+rx_range = (0.000222 * data.time_multiplier, 0.000226 * data.time_multiplier)
+print(rx_range[0], rx_range[1])
+print("samples: ", len(delta))
+for i in range(5):
+    print(delta[i])
+intervals = calculate_tx_intervals(delta)
+data.save([[i] for i in intervals], "out.csv")
+print("Number of intervals: ", len(intervals))
---
+++ logicData.py
@@ -0,0 +1,76 @@
+import csv
+import saleae
+import sys
+
+
+class LogicData:
+    time_multiplier = ""
+    decimal_points = 2
+    new_infile_name = "" #"pycapture.csv"
+    raw_data = []
+
+    def save_py3(self, data, out_file_name):
+        csv_file = open(out_file_name, 'w', newline='') # for python 3
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerows(data)
+        csv_file.close()
+        print("data saved")
+
+    def save_py2(self, data, out_file_name):
+        with open(out_file_name, 'wb') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(data)
+        csvfile.close()
+        print("data saved")
+        
+    def set_time_multiplier(self, unit):
+        if unit == "s":
+            self.time_multiplier = 1
+        elif unit == "ms":
+            self.time_multiplier = 10**3
+        elif unit == "us":
+            self.time_multiplier = 10**6
+
+    #----------------------------------------------------------------#
+
+    def __init__(self, infile_name, time_multiplier):
+        self.new_infile_name = infile_name
+        self.set_time_multiplier(time_multiplier)
+
+        # Load data
+        csv_file = open(infile_name, 'r')
+        reader = csv.reader(csv_file, delimiter=',')
+        next(reader)
+        self.raw_data = [float(line[0]) * self.time_multiplier for line in reader]
+        csv_file.close()
+        print("data loaded")
+
+    # Get durations between each toggle in the data
+    def get_delta_times(self):
+        delta_times = []
+        for i in range(0, len(self.raw_data)-1):
+            delta_time = float(self.raw_data[i+1]) - float(self.raw_data[i])
+            formatting = "%." + str(self.decimal_points) + "f"
+            delta_times.append(formatting % delta_time)
+        return delta_times
+
+    # Save data to output file. Data must be 2-dimentional array (row and col)
+    def save(self, data, out_file_name):
+        if sys.hexversion > 35000000:
+            self.save_py3(data, out_file_name)
+        else:
+            self.save_py2(data, out_file_name)
+
+    # Start capture from Logic and export data to file
+    def capture(self, capture_seconds, amount_of_channels, infile_path):
+        s = saleae.Saleae()
+        s.set_capture_seconds(capture_seconds)
+        s.set_active_channels([i for i in range(amount_of_channels)], [])
+        s.set_sample_rate(s.get_all_sample_rates()[0])
+        print("capturing... (" + str(capture_seconds) + " seconds)")
+        s.capture_start_and_wait_until_finished()
+        s.export_data2(infile_path + self.new_infile_name)
+        print("data exported from Logic")
+
+    def set_decimal_points(self, decimal_points):
+        self.decimal_points = decimal_points
