26711f01135cdd27f5c2e868a38989ea7ad3a488
Jarl Victor Bjørgan <JVBjorgan@gmail.com>
2017-05-15 19:43:22 +0200

    fix

--- lazy_test.py
+++ lazy_test.py
@@ -1,24 +1,25 @@
 import logicData
 
-infile_path = "C:/Users/JarlVictor/Dropbox/Code/2017/capture/"
+infile_path = "C:/Users/JarlVictor/Dropbox/Code/2017/capture/phoenix/"
 inFile = infile_path + "lazy_test.csv"
 
 scanner_lazy = []
 transmit_lazy = []
 
+logicData.capture(10, 8, inFile)
 data = logicData.LogicData(inFile, "ms", 1)
 data.set_decimal_points(2)
-#logicData.capture(60*2, 8, inFile)
 capture_data = data.get_raw_data()
 
 last_timeout = 0
 for i in capture_data:
-    current_timeout = i[1] >> 6
+    current_timeout = i[1] & 0b00000011
     if current_timeout is not last_timeout:
         if current_timeout & 0b01:  # if current_timeout is scanner timeout
-            scanner_lazy.append(i[1] & 0b00111000)
+            scanner_lazy.append((i[1] & 0b00011100) >> 2)
         elif current_timeout & 0b10:  # if current_timeout is transmit timeout
-            transmit_lazy.append(i[1] & 0b00000111)
+            transmit_lazy.append((i[1] & 0b11100000) >> 5)
     last_timeout = current_timeout
 
-print("scanner lazy:", scanner_lazy, "transmit lazy:", transmit_lazy)
+print("scanner lazy:", scanner_lazy)
+print("transmit lazy:", transmit_lazy)
