6ea4afd0863c6be13eb72328c92529cf0c464e59
Jarl Victor Bjørgan <JVBjorgan@gmail.com>
2017-05-15 18:56:12 +0200

    fix

--- lazy_test.py
+++ lazy_test.py
@@ -8,13 +8,17 @@ transmit_lazy = []
 
 data = logicData.LogicData(inFile, "ms", 1)
 data.set_decimal_points(2)
-logicData.capture(60*2, 8, inFile)
+#logicData.capture(60*2, 8, inFile)
 capture_data = data.get_raw_data()
 
+last_timeout = 0
 for i in capture_data:
-    if i[1] >> 6 == 1:
-        scanner_lazy.append(i[1] & 0b00111000)
-    elif i[1] >> 6 == 2:
-        transmit_lazy.append(i[1] & 0b00000111)
+    current_timeout = i[1] >> 6
+    if current_timeout is not last_timeout:
+        if current_timeout & 0b01:  # if current_timeout is scanner timeout
+            scanner_lazy.append(i[1] & 0b00111000)
+        elif current_timeout & 0b10:  # if current_timeout is transmit timeout
+            transmit_lazy.append(i[1] & 0b00000111)
+    last_timeout = current_timeout
 
 print("scanner lazy:", scanner_lazy, "transmit lazy:", transmit_lazy)
