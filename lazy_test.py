fb86aca5aa025d72672be7855e3a67f60923ebf9
Jarl Victor Bjørgan <JVBjorgan@gmail.com>
2017-05-15 18:22:12 +0200

    lazy test

--- lazy_test.py
+++ lazy_test.py
@@ -1,6 +1,20 @@
 import logicData
 
-infile_path = "C:/Users/JarlV/Dropbox/Code/2017/capture/"
-inFile = infile_path + "test.csv"
+infile_path = "C:/Users/JarlVictor/Dropbox/Code/2017/capture/"
+inFile = infile_path + "lazy_test.csv"
 
-logicData.capture(20, 2, inFile)
+scanner_lazy = []
+transmit_lazy = []
+
+data = logicData.LogicData(inFile, "ms", 1)
+data.set_decimal_points(2)
+logicData.capture(60*2, 8, inFile)
+capture_data = data.get_raw_data()
+
+for i in capture_data:
+    if i[1] >> 6 == 1:
+        scanner_lazy.append(i[1] & 0b00111000)
+    elif i[1] >> 6 == 2:
+        transmit_lazy.append(i[1] & 0b00000111)
+
+print("scanner lazy:", scanner_lazy, "transmit lazy:", transmit_lazy)
