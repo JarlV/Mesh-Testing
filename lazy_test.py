cd9e6895e762969af10111dd26575d815899032a
Jarl Victor Bjørgan <JVBjorgan@gmail.com>
2017-05-17 01:10:24 +0200

    adjusted the trickle test

--- lazy_test.py
+++ lazy_test.py
@@ -28,6 +28,30 @@ for i in capture_data:
         transmit_delta_times.append(i[0])
     last_sample = i[1]
 
+# Trickle test -------------------------------
+
+imin = 100
+imax = 2000
+
+def validate_trickle(data,imin,imax):
+    interval = imin
+    last_interval = 0
+    fail_count = 0
+    for time in data:
+        if interval/2 <= time < interval + last_interval/2:
+            pass
+        elif imin/2 <= time < imin + last_interval/2:
+            interval = imin
+        else:
+            print("     test fail on", time, "for interval", interval)
+            fail_count += 1
+        if interval*2 < imax:
+            interval *= 2
+        else:
+            interval = imax
+        last_interval = interval
+    print("Amount of anomalies in intervals: ", fail_count, "out of", len(transmit_delta_times), "total.")   
+
 def validate_intervals(data, imin, imax):
     interval = imin
     fail_count = 0
@@ -45,10 +69,36 @@ def validate_intervals(data, imin, imax):
             interval = imax
     print("Amount of anomalies in intervals: ", fail_count, "out of", len(transmit_delta_times), "total.")
 
-validate_intervals(logicData.get_delta_times(transmit_delta_times),
-                   100,
-                   2000)
+validate_trickle(logicData.get_delta_times(transmit_delta_times),
+                   imin,
+                   imax)
 
+trickle_out_data = [[i] for i in logicData.get_delta_times(transmit_delta_times)]
+trickle_min_vals = [imin/2]
+trickle_max_vals = [imin-1]
+next_min = imin/2
+next_max = imin-1
+last_i = 0
+for i in range(len(trickle_out_data)):
+    if next_max*2 < imax:
+        next_max *= 2
+        trickle_max_vals.append(next_max + last_i/2)
+    else:
+        trickle_max_vals.append(imax-1 + last_i/2)
+    last_i = next_max
+    if next_min*2 < imax/2:
+        next_min *= 2
+        trickle_min_vals.append(next_min)
+    else:
+        trickle_min_vals.append(imax/2)
+
+print(len(trickle_out_data), len(trickle_min_vals), len(trickle_max_vals))
+
+for i in range(len(trickle_out_data)):
+    trickle_out_data[i] = trickle_out_data[i] + [trickle_min_vals[i]] + [trickle_max_vals[i]]
+
+
+logicData.save(trickle_out_data, outFile + 'trickle.csv')
 logicData.save(scanner_lazy, outFile + 'scanner_lazy.csv')
 logicData.save(transmit_lazy, outFile + 'transmit_lazy.csv')
 
