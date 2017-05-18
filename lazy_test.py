5488821bdf8c4521ddc498adadfc41d20ae42240
Jarl Victor Bjørgan <JVBjorgan@gmail.com>
2017-05-19 00:37:45 +0200

    bug fixes

--- lazy_test.py
+++ lazy_test.py
@@ -14,7 +14,7 @@ transmit_lazy = []
 data = logicData.LogicData(inFile, "ms", 8)
 data.set_decimal_points(2)
 capture_data = data.get_raw_data()
-transmit_delta_times = [capture_data[0][0]]
+transmit_times = [capture_data[0][0]]
 
 i_toggles = []
 t_toggles = []
@@ -22,18 +22,18 @@ t_toggles = []
 last_sample = 0
 for i in capture_data:
     current_transmit_lazy = i[1] & 0b111
-    current_transmit_timeout = ((i[1] >> 3) & 1) ^ ((last_sample >> 3) & 1)
-    current_scanning_lazy = (i[1] >> 4) & 0b111
-    current_scanning_timeout = (i[1] >> 7) ^ (last_sample >> 7)
+    current_transmit_timeout = (i[1] >> 8) ^ (last_sample >> 8)
+    current_scanning_lazy = (i[1] >> 3) & 0b111
+    current_scanning_timeout = ((i[1] >> 6) & 1) ^ ((last_sample >> 6) & 1)
     if current_scanning_timeout:  # if scanner timeout
-        scanner_lazy.append([i[0],current_scanning_lazy])
+        scanner_lazy.append([i[0], current_scanning_lazy])
     elif current_transmit_timeout:  # if transmit timeout
         transmit_lazy.append([i[0], current_transmit_lazy])
-        transmit_delta_times.append(i[0])
+        transmit_times.append(i[0])
     if (i[1] >> 7) & 1 is not (last_sample >> 7) & 1:
         i_toggles.append(i[0])
     if i[1] >> 8 is not last_sample >> 8:
-        t_toggles.append(i[10])
+        t_toggles.append(i[0])
     last_sample = i[1]
 
 # Trickle test -------------------------------
@@ -41,11 +41,13 @@ for i in capture_data:
 imin = 100
 imax = 2000
 
+print("--------- test t and i ---------")
+trickle.test_t_and_i(i_toggles, t_toggles)
 
 trickle_out_data = \
     trickle.determine_min_max(imin,
                               imax,
-                              logicData.get_delta_times(transmit_delta_times))
+                              logicData.get_delta_times(transmit_times))
 
 
 logicData.save(trickle_out_data, outFile + 'trickle.csv')
--- trickle.py
+++ trickle.py
@@ -1,4 +1,4 @@
-import logicData
+
 
 def trickle_validate(data, min_vals, max_vals):
     fail_count = 0
@@ -9,6 +9,7 @@ def trickle_validate(data, min_vals, max_vals):
             print("     test fail on", data[i], "not in range [", min_vals[i], ",", max_vals[i], "]")
             fail_count += 1
     print("fail count: ", fail_count)
+
 
 def validate_intervals(data, imin, imax):
     interval = imin
@@ -26,17 +27,17 @@ def validate_intervals(data, imin, imax):
         else:
             interval = imax
 
+# tests t with respect to actual i
 def test_t_and_i(i_toggles, t_toggles):
     for i in range(1,len(i_toggles)):
         if i_toggles[i-1] <= t_toggles[i-1] < i_toggles[i]:
             pass
         else:
-            print("fail.", t_toggles[i-1], "not in range", i_toggles[i-1], ",", i_toggles[i])
+            print(i, "fail.", t_toggles[i-1], "not in range", i_toggles[i-1], ",", i_toggles[i])
 
 
 def determine_min_max(imin, imax, transmit_delta_times):
-    print(transmit_delta_times)
-    trickle_out_data = [[i] for i in logicData.get_delta_times(transmit_delta_times)]
+    trickle_out_data = [[i] for i in transmit_delta_times]
     trickle_min_vals = [imin/2]
     trickle_max_vals = [imin-1]
     next_min = imin/2
@@ -46,10 +47,13 @@ def determine_min_max(imin, imax, transmit_delta_times):
     for i in range(len(trickle_out_data)):
         if next_max*2 < imax:
             next_max *= 2
-            trickle_max_vals.append(next_max + last_i/2)
+            trickle_max_vals.append(next_max + last_i/2)# last_i/2)
         else:
-            trickle_max_vals.append(imax-1 + last_i/2)
+            next_max = imax
+            trickle_max_vals.append(imax-1 + last_i/2)# last_i/2)
+
         last_i = next_max
+
         if next_min*2 < imax/2:
             next_min *= 2
             trickle_min_vals.append(next_min)
@@ -59,6 +63,7 @@ def determine_min_max(imin, imax, transmit_delta_times):
     for i in range(len(trickle_out_data)):
         trickle_out_data[i] = trickle_out_data[i] + [trickle_min_vals[i]] + [trickle_max_vals[i]]
 
+    print("--------- test t with expected i values ---------")
     trickle_validate(transmit_delta_times, trickle_min_vals, trickle_max_vals)
 
     return trickle_out_data
