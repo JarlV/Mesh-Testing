7496e0f35f4223ae96aefe2f4f78b2b9601c7174
Jarl Victor <jvbjorgan@gmail.com>
2017-05-18 16:30:44 +0200

    test changes

--- lazy_test.py
+++ lazy_test.py
@@ -1,6 +1,7 @@
 # Run test:
 # lazy_test.py <.csv capture data>
 
+import trickle
 import logicData
 import sys
 
@@ -15,6 +16,9 @@ data.set_decimal_points(2)
 capture_data = data.get_raw_data()
 transmit_delta_times = [capture_data[0][0]]
 
+i_toggles = []
+t_toggles = []
+
 last_sample = 0
 for i in capture_data:
     current_transmit_lazy = i[1] & 0b111
@@ -26,6 +30,10 @@ for i in capture_data:
     elif current_transmit_timeout:  # if transmit timeout
         transmit_lazy.append([i[0], current_transmit_lazy])
         transmit_delta_times.append(i[0])
+    if (i[1] >> 7) & 1 is not (last_sample >> 7) & 1:
+        i_toggles.append(i[0])
+    if i[1] >> 8 is not last_sample >> 8:
+        t_toggles.append(i[10])
     last_sample = i[1]
 
 # Trickle test -------------------------------
@@ -33,69 +41,11 @@ for i in capture_data:
 imin = 100
 imax = 2000
 
-def validate_trickle(data,imin,imax):
-    interval = imin
-    last_interval = 0
-    fail_count = 0
-    for time in data:
-        if interval/2 <= time < interval + last_interval/2:
-            pass
-        elif imin/2 <= time < imin + last_interval/2:
-            interval = imin
-        else:
-            print("     test fail on", time, "for interval", interval)
-            fail_count += 1
-        if interval*2 < imax:
-            interval *= 2
-        else:
-            interval = imax
-        last_interval = interval
-    print("Amount of anomalies in intervals: ", fail_count, "out of", len(transmit_delta_times), "total.")   
 
-def validate_intervals(data, imin, imax):
-    interval = imin
-    fail_count = 0
-    for time in data:
-        if interval/2 <= time < interval:
-            pass
-        elif imin/2 <= time < imin:
-            interval = imin
-        else:
-            print("     test fail on", time, "for interval", interval)
-            fail_count += 1
-        if interval*2 < imax:
-            interval *= 2
-        else:
-            interval = imax
-    print("Amount of anomalies in intervals: ", fail_count, "out of", len(transmit_delta_times), "total.")
-
-validate_trickle(logicData.get_delta_times(transmit_delta_times),
-                   imin,
-                   imax)
-
-trickle_out_data = [[i] for i in logicData.get_delta_times(transmit_delta_times)]
-trickle_min_vals = [imin/2]
-trickle_max_vals = [imin-1]
-next_min = imin/2
-next_max = imin-1
-last_i = 0
-for i in range(len(trickle_out_data)):
-    if next_max*2 < imax:
-        next_max *= 2
-        trickle_max_vals.append(next_max + last_i/2)
-    else:
-        trickle_max_vals.append(imax-1 + last_i/2)
-    last_i = next_max
-    if next_min*2 < imax/2:
-        next_min *= 2
-        trickle_min_vals.append(next_min)
-    else:
-        trickle_min_vals.append(imax/2)
-
-print(len(trickle_out_data), len(trickle_min_vals), len(trickle_max_vals))
-
-for i in range(len(trickle_out_data)):
-    trickle_out_data[i] = trickle_out_data[i] + [trickle_min_vals[i]] + [trickle_max_vals[i]]
+trickle_out_data = \
+    trickle.determine_min_max(imin,
+                              imax,
+                              logicData.get_delta_times(transmit_delta_times))
 
 
 logicData.save(trickle_out_data, outFile + 'trickle.csv')
---
+++ trickle.py
@@ -0,0 +1,64 @@
+import logicData
+
+def trickle_validate(data, min_vals, max_vals):
+    fail_count = 0
+    for i in range(len(data)):
+        if min_vals[i] <= data[i] < max_vals[i]:
+            pass
+        else:
+            print("     test fail on", data[i], "not in range [", min_vals[i], ",", max_vals[i], "]")
+            fail_count += 1
+    print("fail count: ", fail_count)
+
+def validate_intervals(data, imin, imax):
+    interval = imin
+    fail_count = 0
+    for time in data:
+        if interval/2 <= time < interval:
+            pass
+        elif imin/2 <= time < imin:
+            interval = imin
+        else:
+            print("     test fail on", time, "for interval", interval)
+            fail_count += 1
+        if interval*2 < imax:
+            interval *= 2
+        else:
+            interval = imax
+
+def test_t_and_i(i_toggles, t_toggles):
+    for i in range(1,len(i_toggles)):
+        if i_toggles[i-1] <= t_toggles[i-1] < i_toggles[i]:
+            pass
+        else:
+            print("fail.", t_toggles[i-1], "not in range", i_toggles[i-1], ",", i_toggles[i])
+
+
+def determine_min_max(imin, imax, transmit_delta_times):
+    print(transmit_delta_times)
+    trickle_out_data = [[i] for i in logicData.get_delta_times(transmit_delta_times)]
+    trickle_min_vals = [imin/2]
+    trickle_max_vals = [imin-1]
+    next_min = imin/2
+    next_max = imin-1
+    last_i = 0
+
+    for i in range(len(trickle_out_data)):
+        if next_max*2 < imax:
+            next_max *= 2
+            trickle_max_vals.append(next_max + last_i/2)
+        else:
+            trickle_max_vals.append(imax-1 + last_i/2)
+        last_i = next_max
+        if next_min*2 < imax/2:
+            next_min *= 2
+            trickle_min_vals.append(next_min)
+        else:
+            trickle_min_vals.append(imax/2)
+
+    for i in range(len(trickle_out_data)):
+        trickle_out_data[i] = trickle_out_data[i] + [trickle_min_vals[i]] + [trickle_max_vals[i]]
+
+    trickle_validate(transmit_delta_times, trickle_min_vals, trickle_max_vals)
+
+    return trickle_out_data
