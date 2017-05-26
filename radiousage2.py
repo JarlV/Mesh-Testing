90b64c38c6df3bba6eba634fa8620ce93cfde8c3
Jarl Victor Bjørgan <JVBjorgan@gmail.com>
2017-05-26 20:14:30 +0200

    new test for radio usage done (returns about 50% radio usage)

--- radiousage2.py
+++ radiousage2.py
@@ -3,7 +3,7 @@ import sys
 import matplotlib.pyplot as plt
 
 inFile = sys.argv[1]
-data = logicData.LogicData(inFile, 'ms', 4)
+data = logicData.LogicData(inFile, 'ms', 2)
 data.set_decimal_points(10)
 capture_data = data.get_raw_data()
 
@@ -18,14 +18,15 @@ for i in capture_data:
 
     if disable_toggled:
         active_time = i[0] - last_ready_toggle[0]
-        if last_ready_toggle[1] & 1:
+        if ready:
             rx += active_time
         else:
             tx += active_time
 
     last_sample = i
-    if ready_toggled:
+    if ready_toggled and i[0] - last_ready_toggle[0] > 0.001:
         last_ready_toggle = i
 radio_up_time = rx + tx
 
-print("Radio usage: ", 100 * radio_up_time /capture_data[-1][0], "%")
+print("Radio usage (Tx): ", 100 * tx / capture_data[-1][0], "%")
+print("Radio usage (Rx): ", 100 * rx / capture_data[-1][0], "%")
